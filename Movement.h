5439fad6bdafcc6b7bb835c3aff087ccb7427e68
oheaney380 <37104157+oheaney380@users.noreply.github.com>
2018-03-20 17:32:23 +0000

    Added line spacing in Movement.h

--- Movement.h
+++ Movement.h
@@ -7,15 +7,20 @@
 
 #ifndef MOVEMENT_H
 #define	MOVEMENT_H
+
 //Function to initialise timer0 registers for ScanWithRange
 void initTimer(void);
+
 // Function that delays for given no. of seconds
 void delay_s(char seconds);
+
 // Function similar to delay in seconds, but for a 1/10th of a second
 void delay_tenth_s(char tenth_seconds);
+
 // Scans left, ahead and right for IR signal, turns to signal of greater strength
 // This is all done while keeping previous motion action
 char ScanIR(struct DC_motor *mL, struct DC_motor *mR);
+
 // Scans a range for a number of loops, make sure the total time for this scan
 // is under 8 seconds or timer1 within ScanWithRange will fail.
 // Once accurate direction is found, function will return DirectionFound=2, 
